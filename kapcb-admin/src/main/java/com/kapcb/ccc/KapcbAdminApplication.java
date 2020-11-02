899e41ceb9d4d87f98f28dd519751e27a924336a
Eirccc <eircccallroot@yeah.net>
2020-11-02 21:19:19 +0800

    init

---
+++ kapcb-admin/src/main/java/com/kapcb/ccc/KapcbAdminApplication.java
@@ -0,0 +1,21 @@
+package com.kapcb.ccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <a>Title:KapcbAdminApplication</a>
+ * <a>Author：ccc<a>
+ * <a>Description：<a>
+ *
+ * @author ccc
+ * @version 1.0.0
+ * @date 2020/11/2 21:17
+ */
+@SpringBootApplication
+public class KapcbAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KapcbAdminApplication.class, args);
+    }
+}
---
+++ kapcb-admin/src/test/java/com/kapcb/ccc/KapcbAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KapcbAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
