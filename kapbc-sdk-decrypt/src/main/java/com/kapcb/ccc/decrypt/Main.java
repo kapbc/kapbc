a0e2b3d7886b287bbe8796faebdb4cccba12d85d
kapcb <2530628867@qq.com>
2022-10-25 22:37:33 +0800

    add kapbc-sdk-decrypt

---
+++ kapbc-sdk-decrypt/src/main/java/com/kapcb/ccc/decrypt/Main.java
@@ -0,0 +1,17 @@
+package com.kapcb.ccc.decrypt;
+
+/**
+ * <a>Title: ${NAME} </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: ${NAME} <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date ${DATE} ${TIME}
+ * @since 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ kapbc-sdk-decrypt/src/main/java/com/kapcb/ccc/decrypt/utils/DecryptUtil.java
@@ -0,0 +1,16 @@
+package com.kapcb.ccc.decrypt.utils;
+
+/**
+ * <a>Title: DecryptUtil </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: DecryptUtil <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/10/25 22:27
+ * @since 1.0
+ */
+public class DecryptUtil {
+
+
+}
